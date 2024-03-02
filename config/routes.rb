bf4202928c50dcf8a2edad0ec9d27602793ff929
Raj Kamal <rajkamal879@gmail.com>
2024-03-02 17:41:23 +0530

    added course fetch api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     # writing custom route in order to have readable url
     post '/course/create', to: 'courses#create_with_tutors'
+    get '/courses/fetch', to: 'courses#fetch_courses_with_tutors'
   end
 end
